c381df726a8e3412601255424eef35333569aa47
katerina-kis <katerina-kis@yandex.ru>
2021-04-16 17:16:38 +0300

    дата раньше срока, имя латиницей, неправильный тел, нет галочки под клавиаткру mac

--- src/test/java/ru/netology/CardDelivery.java
+++ src/test/java/ru/netology/CardDelivery.java
@@ -37,6 +37,61 @@ public class CardDelivery {
         $(".notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно забронирована на " + newDate));
 
     }
+    @Test
+    void shouldDate() {
+        open("http://localhost:9999");
+        $$("[type=text]").first().setValue("Москва");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
+        String newDate = plusDays(2).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[placeholder='Дата встречи']").setValue(newDate);
+        $("[data-test-id=name] [type=text]").setValue("Перегудова Екатерина");
+        $("[name=phone]").setValue("+79091231212");
+        $(".checkbox__box").click();
+        $("div.form-field>[type=button]").submit();
+        $("fieldset > div:nth-child(2) > span > span > span > span > span.input__sub").shouldHave(exactText("Заказ на выбранную дату невозможен"));
+
+    }
+@Test
+void shouldName() {
+    open("http://localhost:9999");
+    $$("[type=text]").first().setValue("Москва");
+    $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
+    String newDate = plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    $("[placeholder='Дата встречи']").setValue(newDate);
+    $("[data-test-id=name] [type=text]").setValue("Peregudova Ekaterina");
+    $("[name=phone]").setValue("+79091231212");
+    $(".checkbox__box").click();
+    $("div.form-field>[type=button]").submit();
+    $("div:nth-child(3) > span > span > span.input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+}
+    @Test
+    void shouldTel() {
+        open("http://localhost:9999");
+        $$("[type=text]").first().setValue("Москва");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
+        String newDate = plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[placeholder='Дата встречи']").setValue(newDate);
+        $("[data-test-id=name] [type=text]").setValue("Перегудова Екатерина");
+        $("[name=phone]").setValue("+790912312120");
+        $(".checkbox__box").click();
+        $("div.form-field>[type=button]").submit();
+        $("div:nth-child(4) > span > span > span.input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+    }
+    @Test
+    void shouldNotCheckbox() {
+        open("http://localhost:9999");
+        $$("[type=text]").first().setValue("Москва");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
+        String newDate = plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[placeholder='Дата встречи']").setValue(newDate);
+        $("[data-test-id=name] [type=text]").setValue("Перегудова Екатерина");
+        $("[name=phone]").setValue("+790912312120");
+        $("div.form-field>[type=button]").submit();
+        $(".checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+
+    }
 
 
 
