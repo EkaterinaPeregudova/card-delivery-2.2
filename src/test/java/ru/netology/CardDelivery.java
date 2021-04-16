3533da19a226ad226e6ce4dceda087f7d99fae9c
katerina-kis <katerina-kis@yandex.ru>
2021-04-16 17:19:58 +0300

    реформат кода

--- src/test/java/ru/netology/CardDelivery.java
+++ src/test/java/ru/netology/CardDelivery.java
@@ -9,10 +9,7 @@ import java.time.format.DateTimeFormatter;
 
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
-import static org.openqa.selenium.Keys.*;
-
 
 
 public class CardDelivery {
@@ -37,6 +34,7 @@ public class CardDelivery {
         $(".notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно забронирована на " + newDate));
 
     }
+
     @Test
     void shouldDate() {
         open("http://localhost:9999");
@@ -51,20 +49,22 @@ public class CardDelivery {
         $("fieldset > div:nth-child(2) > span > span > span > span > span.input__sub").shouldHave(exactText("Заказ на выбранную дату невозможен"));
 
     }
-@Test
-void shouldName() {
-    open("http://localhost:9999");
-    $$("[type=text]").first().setValue("Москва");
-    $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
-    String newDate = plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    $("[placeholder='Дата встречи']").setValue(newDate);
-    $("[data-test-id=name] [type=text]").setValue("Peregudova Ekaterina");
-    $("[name=phone]").setValue("+79091231212");
-    $(".checkbox__box").click();
-    $("div.form-field>[type=button]").submit();
-    $("div:nth-child(3) > span > span > span.input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
 
-}
+    @Test
+    void shouldName() {
+        open("http://localhost:9999");
+        $$("[type=text]").first().setValue("Москва");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
+        String newDate = plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[placeholder='Дата встречи']").setValue(newDate);
+        $("[data-test-id=name] [type=text]").setValue("Peregudova Ekaterina");
+        $("[name=phone]").setValue("+79091231212");
+        $(".checkbox__box").click();
+        $("div.form-field>[type=button]").submit();
+        $("div:nth-child(3) > span > span > span.input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+    }
+
     @Test
     void shouldTel() {
         open("http://localhost:9999");
@@ -79,6 +79,7 @@ void shouldName() {
         $("div:nth-child(4) > span > span > span.input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
 
     }
+
     @Test
     void shouldNotCheckbox() {
         open("http://localhost:9999");
@@ -92,7 +93,6 @@ void shouldName() {
         $(".checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
 
     }
-
 
 
 }
